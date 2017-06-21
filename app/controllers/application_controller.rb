ca9a25d4d4628d1801b7bc4b6206a13df5669197
ekeast <ekeast328@gmail.com>
2017-06-21 11:38:13 -0400

    routes added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,24 @@ require 'omniauth-facebook'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  @first = Post.order("created_at").first
+
+
   private
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  helper_method :current_user
+  def set_first
+    @first = Post.all.order("created_at")[1]
+  end
 
+  def set_second
+    @second = Post.all.order("created_at")[2]
+  end
+
+  helper_method :current_user
+  helper_method :set_first
+  helper_method :set_second
 end
